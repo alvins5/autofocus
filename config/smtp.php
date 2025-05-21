b6be38a25f82879bc48e56d94401681eb59a1cfe
alvins5 <alvinsa.i.p@gmail.com>
2025-05-21 23:21:58 +0700

    removing some credentials

--- config/smtp.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Contoh konfigurasi SMTP (jika pakai PHPMailer atau library lain)
-$smtp_host = 'autofocus.sijavengerz.tech';
-$smtp_user = 'brownut.321@gmail.com';
-$smtp_pass = 'rwibfuegkrbmicdg';
-$smtp_port = 587; 
-?>
